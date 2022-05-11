5f4e90da2ee9af095ab65248c5d4d498dd9e4184
voncha90 <voncha1990@gmail.com>
2022-05-11 01:42:52 -0500

    commit 2

--- Service_2/NewtonTest.cs
+++ Service_2/NewtonTest.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -6,17 +7,46 @@ using System.Text;
 
 namespace Service_2
 {
-    public class NewtonTest
-    {
-        public  object Deserialize(string path)
-        {
-            var serializer = new JsonSerializer();
+    public class ServiceBClass
 
-            using (var sw = new StreamReader(path))
-            using (var reader = new JsonTextReader(sw))
+    {
+
+        public string GetServiceBClassData()
+        {
+            string result = string.Empty;
+            Account account = new Account
             {
-                return serializer.Deserialize(reader);
-            }
+                Email = "james@example.com",
+                Active = true,
+                CreatedDate = new DateTime(2013, 1, 20, 0, 0, 0, DateTimeKind.Utc),
+                Roles = new List<string>
+    {
+        "User",
+        "Admin"
+    }
+            };
+
+            result = JsonConvert.SerializeObject(account, Formatting.Indented);
+
+
+
+            JObject staff = new JObject();
+            staff.Add(new JProperty("Name", "Jack"));
+            staff.Add(new JProperty("Age", 33));
+            staff.Add(new JProperty("Department", "Personnel Department"));
+            staff.Add(new JProperty("Leader", new JObject(new JProperty("Name", "Tom"), new JProperty("Age", 44), new JProperty("Department", "Personnel Department"))));
+            Console.WriteLine(staff.ToString());
+
+            return result;
+
         }
+    }
+
+    public class Account
+    {
+        public string Email { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public IList<string> Roles { get; set; }
     }
 }
